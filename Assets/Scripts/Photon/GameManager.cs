e6a6da6d2c19017c469eec63271c6961442a8127
ZROS <zros98@gmail.com>
2019-11-27 22:55:35 +0100

    Add co-op test project

---
+++ Assets/Scripts/Photon/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public GameObject PlayerPrefab;
+    private void Start()
+    {
+        Vector3 pos = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+        PhotonNetwork.Instantiate(PlayerPrefab.name, pos, Quaternion.identity);
+    }
+
+    public void Leave()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    public override void OnLeftRoom()
+    {
+        // Когда текущий игрок (мы) покидает комнату
+        SceneManager.LoadScene(0);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.LogFormat("Player {0} entered room", newPlayer.NickName);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.LogFormat("Player {0} left room", otherPlayer.NickName);
+    }
+}
---
+++ Assets/Scripts/Photon/LobbyManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public Text LogText;
+    void Start()
+    {
+        PhotonNetwork.NickName = "Player" + Random.Range(1000, 9999);
+        Log("Player's name is set to" + PhotonNetwork.NickName);
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Log("Connected to Master");
+    }
+    
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions() {MaxPlayers = 2});
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Log("Joined the room");
+        
+        PhotonNetwork.LoadLevel("Game");
+    }
+    
+    void Log(string message)
+    {
+        Debug.Log(message);
+        LogText.text += "\n";
+        LogText.text += message;
+    }
+    
+}
