4fade6b21a7acd3036ce27e231585da3c697ad69
ZROS <zros98@gmail.com>
2019-12-04 20:48:13 +0100

    Add assets

--- Assets/Scripts/CreateMob.cs
+++ Assets/Scripts/CreateMob.cs
@@ -10,13 +10,16 @@ public class CreateMob : MonoBehaviour
 {
     private PhotonView _photonView;
     public GameObject prefab;
-
+    public IPunPrefabPool pun;
 
 
     public void Spawn()
     {
         Vector3 randomPosition = new Vector3(Random.Range(-5, 0), Random.Range(-5, 5));
         //Instantiate(prefab, randomPosition, Quaternion.identity);
-        PhotonNetwork.Instantiate(prefab.name, randomPosition, Quaternion.identity);
+        //PhotonNetwork.Instantiate(prefab.name, randomPosition, Quaternion.identity);
+        //PhotonNetwork.PrefabPool.Instantiate("Mob", randomPosition, Quaternion.identity);
+        pun.Instantiate("Mob", randomPosition, Quaternion.identity);
     }
+    
 }
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private PhotonView _photonView;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!_photonView.IsMine) return;
+        
+        if(Input.GetKey(KeyCode.LeftArrow)) transform.Translate(-Time.deltaTime * 5,0,0);
+        if(Input.GetKey(KeyCode.RightArrow)) transform.Translate(Time.deltaTime * 5,0,0);
+    }
+}
