4fbeb58a0f27b0682a92b03600f439d648edf43d
ZROS <zros98@gmail.com>
2019-12-06 01:07:11 +0100

    Add host

--- Assets/Scripts/CreateMob.cs
+++ Assets/Scripts/CreateMob.cs
@@ -5,20 +5,48 @@ using Photon.Pun;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
 using UnityNightPool;
 using Random = UnityEngine.Random;
 
-public class CreateMob : MonoBehaviour
+public class CreateMob : MonoBehaviour,IPunObservable
 {
+    private PoolObject _poolObject;
+    public string testString;
+    public Text textField;
     public void Spawn()
     {
-        PoolObject platform = PoolManager.Get(1);
+        _poolObject = PoolManager.Get(1);
+        testString = "OK";
+    }
+
+    private void Start()
+    {
+        testString = PhotonNetwork.LocalPlayer.NickName;
+    }
+
+    private void Update()
+    {
+        textField.text = testString;
     }
 
     public void ReturnToPool()
     {
         PoolManager.ReturnPool();
-        
     }
 
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(testString);
+            Debug.Log("writing");
+        }
+        else
+        {
+            testString = (string) stream.ReceiveNext();
+            
+            Debug.Log("reading");
+        }
+    }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,10 +7,13 @@ public class PlayerControls : MonoBehaviour
 {
     private PhotonView _photonView;
     
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         _photonView = GetComponent<PhotonView>();
+        
     }
 
     // Update is called once per frame
@@ -21,4 +24,6 @@ public class PlayerControls : MonoBehaviour
         if(Input.GetKey(KeyCode.LeftArrow)) transform.Translate(-Time.deltaTime * 5,0,0);
         if(Input.GetKey(KeyCode.RightArrow)) transform.Translate(Time.deltaTime * 5,0,0);
     }
+
+    
 }
