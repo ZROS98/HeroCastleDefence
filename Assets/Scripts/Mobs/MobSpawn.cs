1ac49fabb7b7c462a4884ae58acdfeedc8888a6c
ZROS <zros98@gmail.com>
2019-12-17 22:16:58 +0100

    MobMenu alfa version

--- Assets/Scripts/Mobs/MobSpawn.cs
+++ Assets/Scripts/Mobs/MobSpawn.cs
@@ -8,23 +8,29 @@ public class MobSpawn : MonoBehaviourPunCallbacks
     public GameObject mobBox;
     public GameObject mobSpawnArea;
     private PhotonView p_photonView;
+    private int mobID;
 
     private void Start()
     {
         p_photonView = GetComponent<PhotonView>();
     }
 
-    public void Create()
+    public void Create(int mobNumber)
     {
+        mobID = mobNumber;
         p_photonView.RPC("RPC_Create", RpcTarget.AllBuffered);
     }
 
     [PunRPC]
     private void RPC_Create()
     {
-        int countChild = gameObject.transform.childCount;
-        int randomChild = UnityEngine.Random.Range(0, countChild);
-        var randomCharacter = mobBox.transform.GetChild(randomChild);
+        //int countChild = gameObject.transform.childCount;
+        //int randomChild = UnityEngine.Random.Range(0, countChild);
+        //var randomCharacter = mobBox.transform.GetChild(randomChild);
+
+        MobArray mobArray = GetComponent<MobArray>();
+        var randomCharacter = mobArray.mobFilledArray[mobID];
+        
         randomCharacter.transform.position = mobSpawnArea.transform.position;
         int spacePosition = randomCharacter.name.LastIndexOf(' ');
         string childName = randomCharacter.name.Substring(0, spacePosition);
