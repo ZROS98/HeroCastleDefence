fd33a60b6281d8aae8fd82bb0dfb13b806fb80f8
ZROS <zros98@gmail.com>
2019-12-24 19:42:08 +0100

    fix

--- Assets/Scripts/Mobs/StartCreate.cs
+++ Assets/Scripts/Mobs/StartCreate.cs
@@ -7,42 +7,23 @@ using UnityEngine.SceneManagement;
 public class StartCreate : MonoBehaviourPunCallbacks
 {
     public Object[] p_charactersPrefabs;
-    private Transform p_transformMobDefaultSpawnPoint;
+    [SerializeField] private Transform p_transformMobDefaultSpawnPoint;
     private Transform p_transformFinalWayPointPoint;
-    private Dictionary<string, Dictionary<string, GameObject>> dictionaryRegister;
 
     [System.Obsolete]
     void Start()
     {
-        //p_charactersPrefabs = Resources.LoadAll( "", typeof(GameObject));
+        
         foreach (var prefabCharacter in p_charactersPrefabs)
         {
             int spacePosition = prefabCharacter.name.LastIndexOf(' ');
             string childName = prefabCharacter.name.Substring(0, spacePosition);
 
-            GameObject newObjectCharacter = (GameObject) PhotonNetwork.InstantiateSceneObject(prefabCharacter.name, p_transformMobDefaultSpawnPoint.position,
-                p_transformMobDefaultSpawnPoint.rotation);
-     //       CreateMobsDictionarys(childName, newObjectCharacter);
+            GameObject newObjectCharacter = (GameObject) PhotonNetwork.InstantiateSceneObject(prefabCharacter.name, p_transformMobDefaultSpawnPoint.position, Quaternion.identity );
 
             newObjectCharacter.transform.SetParent(transform);
             var childObject = newObjectCharacter.transform.FindChild(childName);
             childObject.GetComponent<SkinnedMeshRenderer>().enabled = false;
-
-//            newObjectCharacter.GetComponent<MobSettings>().transformMobDefaultSpawnPiont = p_transformMobDefaultSpawnPoint;
-//            newObjectCharacter.GetComponent<MobSettings>().transformFinalWayPoint = p_transformFinalWayPointPoint;        
-        }
-
-    }
-
-    private void CreateMobsDictionarys(string dictionaryName, GameObject mob)
-    {
-        if (dictionaryRegister.ContainsKey("Dictionary_"+dictionaryName))
-        {
-            dictionaryRegister["Dictionary_" + dictionaryName].Add(dictionaryName, mob);
-        }
-        else
-        {
-            dictionaryRegister.Add("Dictionary_" + dictionaryName, new Dictionary<string, GameObject>());
         }
     }
 }
