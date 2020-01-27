9953565e05ee0fa16421fae49f536adf50de53b2
ZROS <zros98@gmail.com>
2020-01-27 17:09:20 +0100

    Add movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 15;
+
+    public float speedH = 2.0f;
+
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+
+    private float pitch = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+        Vector3 vectorMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+        gameObject.transform.position += vectorMovement.normalized * moveSpeed * Time.deltaTime;
+    }
+
+    public void CameraRotation()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+        
+        transform.eulerAngles = new Vector3(pitch,yaw, 0.0f);
+    }
+}
