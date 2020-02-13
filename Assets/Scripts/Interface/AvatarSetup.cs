ad5d6eb36dd069811eb52a7a0a58b10ec88efe4e
Elenger <orion96king@gmail.com>
2020-02-13 22:05:14 +0200

    Add spawn and destroy mob

---
+++ Assets/Scripts/Interface/AvatarSetup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class AvatarSetup : MonoBehaviour
+{
+    private PhotonView PV;
+    public GameObject myCharacter;
+    public int characterValue;
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        int spawnPicker = Random.Range(0, GameSetup.GS.spawnPoints.Length);
+        if (PV.IsMine)
+        {
+    //        PV.RPC("RPC_AddCharacter", RpcTarget.AllBuffered, PlayerInfo.PI.mySelectedCharacter);
+            PhotonNetwork.Instantiate(myCharacter.name, GameSetup.GS.spawnPoints[spawnPicker].position, GameSetup.GS.spawnPoints[spawnPicker].rotation, 0);
+        }
+    }
+
+    [PunRPC]
+    void RPC_AddCharacter(int whichCharacter)
+    {
+        characterValue = whichCharacter;
+        myCharacter = Instantiate(PlayerInfo.PI.allCharacters[whichCharacter], transform.position, transform.rotation,
+            transform);
+    }
+        
+}
